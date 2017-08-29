87e634665a0d9bdb05b728ae4e3e3d66e58751e8
jkeiffer1 <saros82@hotmail.com>
2017-08-29 13:41:44 -0400

    added gsub to filter out incorrect characters

--- isbncheck.rb
+++ isbncheck.rb
@@ -1,9 +1,13 @@
 def check_isbn10?(isbn)
-	array = ["1","2","3","4","5","6","7","8","9","x"]
+	isbn.gsub!(/[^0-9A-Za-z]/, "")
+	array = isbn.split(//)
 	if isbn.length == 10
-		isbn == array
 		true
 	else
 		false
 	end
+
 end
+
+p check_isbn10?("123456789x")
+
